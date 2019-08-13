c1c072155ea378fb1f96f953c54d605fe113b67d
Abel David <abel.david@ignitarium.com>
2019-08-13 11:14:12 +0530

    init

--- docker_env/insp/f1teenth/launch_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-xhost +local:
-sudo docker run -it --net=host --privileged\
-  -e DISPLAY=$DISPLAY \
-  -e QT_GRAPHICSSYSTEM=native \
-  -v "/tmp/.X11-unix:/tmp/.X11-unix" \
-  --name=f110sim \
-  f110sim:kinetic
--- docker_env/intel_gpu/launch_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-xhost +local:
-sudo docker run -it --net=host --privileged\
-  -e DISPLAY=$DISPLAY \
-  -e QT_GRAPHICSSYSTEM=native \
-  -v "/tmp/.X11-unix:/tmp/.X11-unix" \
-  -v "/home/$USER/Data/ignitarium/projects/robotics/dev_bot_v1/package:/home/david/vol/lmq_ws/src" \
-  --name=v1_c_sim \
-  v1_dev_bot:melodic
